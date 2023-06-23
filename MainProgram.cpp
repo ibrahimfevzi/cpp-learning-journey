30917313abc164219bc133bef04983c7b010a802
ibrahimfevzi <ibrahimfevzi@gmail.com>
2023-06-24 01:37:02 +0300

    variables

---
+++ MainProgram.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    string isim = "Ali Kaya";
+    int yas = 20;
+
+    cout << "Merhaba" << endl
+         << endl
+         << "Ogrenci Adi - Soyadi : "
+         << isim << endl;
+    cout <<"Ogrenci Yasi : " <<  yas << endl;
+
+    return 0;
+}
