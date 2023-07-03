58ef9d34ef991a7b04df1726c0e6c0714741c64e
ibrahimfevzi <ibrahimfevzi@gmail.com>
2023-07-04 00:48:40 +0300

    try-catch

---
+++ try-catch/Hesaplayici.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+try
+{
+    int yas;
+    cout << "Yaşınızı giriniz: ";
+    cin >> yas;
+    if (yas >= 18)
+    {
+        cout << "Mekana hoşgeldiniz !" << endl;
+    }
+    else{
+        throw (yas);
+        
+    }
+}
+catch(int hataNo)
+{
+    cout << "Mekana giremezsiniz ! Yaşınız 18'den küçük" << endl;
+    cout << "Yaşınız: " << hataNo << endl;
+}
+
+    return 0;
+}
+
+
+
+
+    
